873676a19626f1b00bb3399fdbebd3fb98ea8e1e
devAYZ <fatokimiemman@gmail.com>
2024-07-04 10:07:28 +0100

    add project file

---
+++ MagnifyingGlass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagnifyingGlass
+//
+//  Created by Ayokunle Pro on 7/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
